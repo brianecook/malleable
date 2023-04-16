893f40430a8f8b14362b0fd3373117585fbd83e6
Brian Cook <brianecook@gmail.com>
2023-04-16 09:49:36 -0700

    adds updated select helper code

--- scripts/helpers/_dom.js
+++ scripts/helpers/_dom.js
@@ -1,18 +1,33 @@
 const nodeListPrototype = {
-  listen(event, callback) {
-    if (this.addEventListener) {
-      this.addEventListener(event, callback);
+  execute(fn) {
+    if (this instanceof Element || this instanceof HTMLDocument) {
+      fn(this);
     } else {
       this.forEach((node) => {
-        node.addEventListener(event, callback);
+        fn(node);
       });
     }
   },
+  listen(event, callback) {
+    const callbackWithNode = (node) => () =>
+      callback(Object.assign(node, nodeListPrototype));
+
+    this.execute((node) =>
+      node.addEventListener(event, callbackWithNode(node))
+    );
+  },
+  modifyClass(method, className) {
+    this.execute((node) => node.classList[method](className));
+  },
+  attribute(method, attributeName, value) {
+    this.execute((node) => node.setAttribute(attributeName, value));
+  },
 };
 
-export function select(selector) {
-  let nodeList = document.querySelectorAll(selector);
-  if (nodeList.length === 1) nodeList = nodeList[0];
-  const extendedNodeList = Object.assign(nodeList, nodeListPrototype);
-  return extendedNodeList;
+export function select(selector, parent = document) {
+  const object =
+    typeof selector === 'string' ? parent.querySelectorAll(selector) : selector;
+  const selectObject = object && object.length === 1 ? object[0] : object;
+  const extendedObject = Object.assign(selectObject, nodeListPrototype);
+  return extendedObject;
 }
