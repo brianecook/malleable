02c880d2cbc5cb2067cac8e65ac31a732e6de3f0
Brian Cook <brianecook@gmail.com>
2023-05-17 14:54:01 -0700

    removes unnecessary code

--- scripts/helpers/_money.js
+++ scripts/helpers/_money.js
@@ -1,6 +1,5 @@
 export function formatMoney(cents, currency = 'USD') {
   const centsNumber = Number(cents);
-  const isValidCents = centsNumber.toString().indexOf('.') > -1;
   const dollars = centsNumber / 100;
 
   return Intl.NumberFormat('en-US', {
