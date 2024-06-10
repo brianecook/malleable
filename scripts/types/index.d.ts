e789d1a5a3a93a89a840a3bd40365d5e191f9be5
Brian Cook <brianecook@gmail.com>
2024-06-10 11:23:41 -0700

    removes unused type definitions

--- scripts/types/index.d.ts
+++ scripts/types/index.d.ts
@@ -38,37 +38,6 @@ declare global {
 
 // add additional keys to types as needed
 
-export type OptionsWithValues = {
-  name: string;
-  position: number;
-  values: string[];
-};
-
-// export type Variant = {
-//   available: boolean;
-//   id: number;
-//   title: string;
-//   option1: string | null;
-//   option2: string | null;
-//   option3: string | null;
-//   options: string[];
-//   price: number;
-// };
-
-// export type Image = {
-//   src: string;
-//   width: number;
-//   height: number;
-//   alt: string;
-// };
-
-export type FeaturedImage = {
-  url: string;
-  width: number;
-  height: number;
-  alt: string;
-};
-
 export type Variant = {
   title: string;
   availableForSale: boolean;
@@ -89,40 +58,10 @@ export type Variant = {
   ];
 };
 
-export type Image = {
-  id: string;
-  width: number;
-  height: number;
-  url: string;
-  altText: string | undefined;
-};
-
-export type Option = {
+export type OptionsWithValues = {
   name: string;
+  position: number;
   values: string[];
-};
-
-export type Product = {
-  availableForSale: boolean;
-  id: string;
-  title: string;
-  handle: string;
-  url: string;
-  descriptionHtml: TrustedHTML;
-  featured_image: FeaturedImage;
-  variants: {
-    nodes: [Variant];
-  };
-  images: {
-    nodes: [Image];
-  };
-  options: [Option];
-};
-
-export type EventProduct = {
-  details: Product;
-  options_with_values: OptionsWithValues[];
-  metafields: Metafields;
 };
 
 export type LineItem = {
@@ -142,16 +81,4 @@ export type Cart = {
   item_count: number;
   items_subtotal_price: number;
   items: LineItem[];
-};
-
-export type Metafields = {
-  [key: string]: string;
-};
-
-export type SearchResults = {
-  resources: {
-    results: {
-      products: Product[];
-    };
-  };
 };
